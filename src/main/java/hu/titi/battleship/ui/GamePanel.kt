55339b46e39240945bf1d064bcb87e4d8346d659
Márkus Krisztián <markuskrisztian1@gmail.com>
2017-10-15 21:50:47 +0200

    Single view based game field

--- src/main/java/hu/titi/battleship/ui/GamePanel.kt
+++ src/main/java/hu/titi/battleship/ui/GamePanel.kt
@@ -1,15 +1,19 @@
 package hu.titi.battleship.ui
 
 import android.content.Context
+import android.graphics.Canvas
+import android.graphics.Color
+import android.graphics.Paint
+import android.service.quicksettings.Tile
+import android.util.Log
 import android.view.Gravity
+import android.view.MotionEvent
 import android.view.View
 import android.widget.LinearLayout
 import hu.titi.battleship.model.*
-import org.jetbrains.anko.linearLayout
-import org.jetbrains.anko.matchParent
-import org.jetbrains.anko.runOnUiThread
+import org.jetbrains.anko.*
 import org.jetbrains.anko.sdk23.listeners.onClick
-import org.jetbrains.anko.textView
+import org.jetbrains.anko.sdk23.listeners.onTouch
 import java.util.concurrent.atomic.AtomicBoolean
 
 interface GameView {
@@ -19,123 +23,145 @@ interface GameView {
     fun gameOver(won: Boolean)
 }
 
-private typealias Generator = LinearLayout.(Int, Int) -> View
-
-private val firstRowGenerator: Generator = { x, _ ->
-    textView(x.toString()) {
-        gravity = Gravity.CENTER
-    }
+enum class TileState(val color: Int) {
+    UNKNOWN(Color.WHITE),
+    SHIP(Color.GRAY),
+    MISS(Color.BLUE),
+    HIT(Color.RED)
 }
 
 class GamePanel(context: Context) : LinearLayout(context), GameView, PlayerListener {
 
-    private val gameTiles = Array(SIZE) { y ->
-        Array(SIZE) { x ->
-            GameTile(context, Coordinate(x, y), TileState.UNKNOWN).apply {
-                onClick {
-                    tileClick(coordinate)
-                }
-            }
-        }
-    }
-    //private val rows = Array(SIZE + 1, this::createRow)
-
-    private var enabled = AtomicBoolean(false)
+    private val fieldView = FieldView()
+    @Volatile private var clickEnabled: Boolean = false
     private val store = Store<Coordinate>()
 
+    inner class FieldView : View(context) {
+        private val paints = Array(TileState.values().size) { i ->
+            Paint().apply {
+                color = TileState.values()[i].color
+                style = Paint.Style.FILL
+            }
+        }
+
+        init {
+            layoutParams = LinearLayout.LayoutParams(0, matchParent, SIZE.toFloat())
+            onTouch { v, event ->
+                fieldClick(event)
+                true
+            }
+        }
+
+        override fun onDraw(canvas: Canvas) {
+            val cellWidth = canvas.width / SIZE
+            val cellHeight = canvas.height / SIZE
+
+            for (x in 0 until SIZE) {
+                for (y in 0 until SIZE) {
+                    canvas.drawRect((x * cellWidth).toFloat(),
+                            (y * cellHeight).toFloat(),
+                            ((x + 1) * cellWidth).toFloat(),
+                            ((y + 1) * cellHeight).toFloat(),
+                            paints[fields[x][y].ordinal])
+                }
+            }
+
+        }
+    }
+
+    val fields: Array<Array<TileState>> = Array(SIZE) { x ->
+        Array(SIZE) { y ->
+            TileState.UNKNOWN
+        }
+    }
+
     init {
-        (0..SIZE).forEach(this::createRow)
         orientation = VERTICAL
         layoutParams = LinearLayout.LayoutParams(0, matchParent, 1F)
         weightSum = (SIZE + 1).toFloat()
-    }
 
-    private fun createRow(row: Int) {
+        // Header
         linearLayout {
-            layoutParams = LinearLayout.LayoutParams(matchParent, 0, 1F)
+            layoutParams = LayoutParams(matchParent, 0, 1F)
             weightSum = (SIZE + 1).toFloat()
 
-            val cellParams = LinearLayout.LayoutParams(0, matchParent, 1F)
-
-            val header = if (row == 0) "" else ('A' + row - 1).toString()
-            val generator = if (row == 0) firstRowGenerator else rowGenerator()
-
-            textView(header) {
-                layoutParams = cellParams
-                gravity = Gravity.CENTER
+            view().lparams(width = 0, height = matchParent, weight = 1F)
+            for (i in 0 until SIZE) {
+                textView(text = i.toString()) {
+                    gravity = Gravity.CENTER
+                }.lparams(width = 0, height = matchParent, weight = 1F)
             }
+        }
 
-            for (cell in 1..SIZE) {
-                generator(cell, row).apply {
-                    layoutParams = cellParams
+        linearLayout {
+            layoutParams = LayoutParams(matchParent, 0, SIZE.toFloat())
+
+            verticalLayout {
+                layoutParams = LayoutParams(0, matchParent, 1F)
+                weightSum = SIZE.toFloat()
+
+                for (i in 0 until SIZE) {
+                    textView(text = ('A' + i).toString()) {
+                        gravity = Gravity.CENTER
+                    }.lparams(width = matchParent, height = 0, weight = 1F)
                 }
             }
+
+            addView(fieldView)
+
         }
     }
 
-    private fun tileClick(coordinate: Coordinate) {
-        if (enabled.get()) {
-            store.place(coordinate)
+    private fun fieldClick(event: MotionEvent) {
+        if (clickEnabled && event.action == MotionEvent.ACTION_UP) {
+            val x = minOf((event.x / fieldView.width * SIZE).toInt(), SIZE - 1)
+            val y = minOf((event.y / fieldView.height * SIZE).toInt(), SIZE - 1)
+
+            store.place(Coordinate(x, y))
         }
+    }
+
+    override fun await(prevResult: ShootResult): Coordinate? {
+        clickEnabled = true
+        val coordinate = store.await()
+        clickEnabled = false
+        return coordinate
+    }
+
+    override fun abort() {}
+
+    override fun updateTile(position: Coordinate, state: TileState): Boolean {
+        this[position] = state
+        context.runOnUiThread {
+            Log.i("sad", "invalidating")
+            fieldView.invalidate()
+        }
+        return true
     }
 
     override fun showShips(ships: Collection<Ship>): Boolean {
         context.runOnUiThread {
             ships.flatMap { it.tiles.map(Pair<Coordinate, Boolean>::first) }
-                 .forEach { this@GamePanel[it].state = TileState.SHIP }
+                    .forEach { this@GamePanel[it] = TileState.SHIP }
+            fieldView.invalidate()
         }
-
         return true
-    }
-
-    override fun gameOver(won: Boolean) {}
-
-    override fun abort() {
-        store.place(Coordinate(0, 0))
-    }
-
-    override fun await(prevResult: ShootResult): Coordinate {
-        enabled.set(true)
-        val coord = store.await()
-        enabled.set(false)
-        return coord
     }
 
     override fun unveilShip(ship: Ship): Boolean {
         context.runOnUiThread {
             ship.border().forEach {
-                this@GamePanel[it].state = TileState.MISS
+                this@GamePanel[it] = TileState.MISS
             }
+            fieldView.invalidate()
         }
-
         return true
     }
 
-    override fun updateTile(position: Coordinate, state: TileState): Boolean {
-        context.runOnUiThread {
-            this@GamePanel[position].state = state
-        }
+    override fun gameOver(won: Boolean) {}
 
-        return true
+    private operator fun set(position: Coordinate, value: TileState) {
+        fields[position.x][position.y] = value
     }
 
-    private fun LinearLayout.rowGenerator(): Generator = { x, y ->
-        this@GamePanel[x - 1, y - 1].apply {
-            this@rowGenerator.addView(this)
-        }
-    }
-
-    private operator fun get(position: Coordinate) = gameTiles[position.y][position.x]
-
-    private operator fun get(x: Int, y: Int) = get(Coordinate(x, y))
-
-    /*
-    override fun enable() {
-        enabled.set(true)
-    }
-
-    override fun disable() {
-        enabled.set(false)
-    }
-    */
 }
